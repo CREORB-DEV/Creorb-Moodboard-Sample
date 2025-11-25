36fa56c770e4a4d1f1ec0a4e20479bc5ae3e34ea
MNM <dev@creorb.studio>
2025-11-25 14:56:14 +0530

    few fixes

--- mobile/moodboard-app/App.jsx
+++ mobile/moodboard-app/App.jsx
@@ -1,12 +1,11 @@
-import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 import Toast from 'react-native-toast-message';
 
 import './global.css';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import AppNavigator from 'navigation/AppNavigator';
-import { MoodProvider } from 'context/MoodContext';
-import { AuthProvider } from 'context/AuthContext';
+import AppNavigator from './navigation/AppNavigator';
+import { MoodProvider } from './context/MoodContext';
+import { AuthProvider } from './context/AuthContext';
 
 export default function App() {
   return (
